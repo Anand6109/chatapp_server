7e48f17885cdcc1f7ba06e624d73055afdc3e205
Anand <anand@Anands-MacBook-Air.local>
2026-05-24 02:00:26 +0530

    check on local database

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,6 @@
  const corsOptions = {
   origin: "https://chatapp-frontend-six-omega.vercel.app",
+  // origin: "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
